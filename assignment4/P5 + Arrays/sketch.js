4b485ed7bf3e40df9a9f2120f0a7cd3b98c28e41
julielichaa <Julielichaa@ocadu.ca>
2025-03-14 18:30:16 -0400

    added assignment4

--- assignment4/P5 + Arrays/sketch.js
+++ assignment4/P5 + Arrays/sketch.js
@@ -1,4 +1,4 @@
-// Array of particles
+// Array of particles, each will be an object with more attributes
 let particles = [];
 
 class Particle {
@@ -7,28 +7,35 @@ class Particle {
     this.y = y;
     this.size = random(10, 20);
     this.life = 255; // Transparency of the particle
-    this.speedX = random(-2, 2); 
-    this.speedY = random(-2, 2); 
+    this.speedX = random(-2, 2); // Speed of particle in X direction
+    this.speedY = random(-2, 2); // Speed of particle in Y direction
     this.color = color(random(255), random(255), random(255)); // Random color for each particle
   }
 
+  // Method to update the particle's state
   update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-    this.life -= 5;
-    this.size *= 0.98;
+    this.x += this.speedX; // Move horizontally
+    this.y += this.speedY; // Move vertically
+    this.life -= 5; // Decrease life over time
+    this.size *= 0.98; // Shrink the particle over time
 
-    // Bounce off edges
-    if (this.x < 0 || this.x > width) this.speedX *= -1;
-    if (this.y < 0 || this.y > height) this.speedY *= -1;
+    // If particle hits the edge of the canvas, it bounces back, i love it lol
+    if (this.x < 0 || this.x > width) {
+      this.speedX *= -1;
+    }
+    if (this.y < 0 || this.y > height) {
+      this.speedY *= -1;
+    }
   }
 
+  // Method to display the particle
   display() {
     fill(this.color.levels[0], this.color.levels[1], this.color.levels[2], this.life);
     noStroke();
     ellipse(this.x, this.y, this.size);
   }
 
+  // Method to check if the particle is done (life <= 0)
   isDead() {
     return this.life <= 0;
   }
@@ -40,13 +47,15 @@ function setup() {
 }
 
 function draw() {
-  background(0, 50);
+  background(0, 50); // Fade effect
 
+  // Update and display all particles
   for (let i = particles.length - 1; i >= 0; i--) {
     let p = particles[i];
     p.update();
     p.display();
 
+    // Remove particle when its life is over
     if (p.isDead()) {
       particles.splice(i, 1);
     }
@@ -54,16 +63,19 @@ function draw() {
 }
 
 function mousePressed() {
+  // Push a new particle to the array when the mouse is pressed
   particles.push(new Particle(mouseX, mouseY));
-  console.log(particles);
+  console.log(particles); // Log the current particles array to console for reference
 }
 
+// Using array methods like join() and concat()
 function mouseReleased() {
   let particlePositions = particles.map(p => `(${p.x}, ${p.y})`);
-  console.log("Particle Positions: " + particlePositions.join(", "));
+  let joinedPositions = particlePositions.join(", ");
+  console.log("Particle Positions: " + joinedPositions);
 }
 
-// Display a message on page load
+// Extra: Handling an HTML element (adding a message when the user clicks)
 document.addEventListener("DOMContentLoaded", function() {
   const messageDiv = document.createElement("div");
   messageDiv.style.position = "absolute";
@@ -76,4 +88,3 @@ document.addEventListener("DOMContentLoaded", function() {
   messageDiv.innerHTML = "Click anywhere to create particles!";
   document.body.appendChild(messageDiv);
 });
-
